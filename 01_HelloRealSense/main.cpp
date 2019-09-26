3c81b1ed6e2c6dbee298a5e74dd5aad050528638
Daniel Packard <pack3754@gmail.com>
2019-09-26 09:33:17 -0500

    Minor cleanup

--- 01_HelloRealSense/main.cpp
+++ 01_HelloRealSense/main.cpp
@@ -10,7 +10,8 @@ int main()
     rs2::context ctx;
 
     std::cout << "hello from librealsense - " << RS2_API_VERSION_STR << std::endl;
-    std::cout << "You have " << ctx.query_devices().size() << " RealSense devices connected" << std::endl;
+    std::cout << "You have " << ctx.query_devices ().size ()
+              << " RealSense devices connected\n";
 
     return 0;
 }
