fae401eb87136d40ddb907bb54dc8c33dfe856d9
Daniel Packard <daniel.packard@complexrehabsystems.com>
2019-10-03 15:20:18 -0500

    Add basic pcl viewer

---
+++ 06_Viewer/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/console/time.h>
+
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud<PointT> PointCloudT;
+
+int main (int argc, char* argv[]) 
+{
+  PointCloudT::Ptr cloud_in (new PointCloudT);  // Original point cloud
+
+  if (argc < 2)
+  {
+    printf ("Usage :\n");
+    printf ("\t\t%s file.ply number_of_ICP_iterations\n", argv[0]);
+    PCL_ERROR ("Provide one ply file.\n");
+    return (-1);
+  }
+
+  if (pcl::io::loadPLYFile (argv[1], *cloud_in) < 0)
+  {
+    PCL_ERROR ("Error loading cloud %s.\n", argv[1]);
+    return (-1);
+  }
+  pcl::visualization::PCLVisualizer viewer ("ICP demo");
+  viewer.addPointCloud (cloud_in, "cloud_in_v1");
+
+  // Display the visualiser
+  while (!viewer.wasStopped ())
+  {
+    viewer.spinOnce ();
+  }
+  return (0);
+}
